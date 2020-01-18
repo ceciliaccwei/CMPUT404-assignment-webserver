1047dc7fd810960b0863ea2e7d1f6557479733a2
ceciliaccwei <ceciliawei@yahoo.com>
2020-01-18 14:22:25 -0700

    comment done

--- server.py
+++ server.py
@@ -43,46 +43,60 @@ import os
 class MyWebServer(socketserver.BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(1024).strip()
+        # Parse the data
         data = self.data.decode().split('\r\n')
         http_request = data[0].split()
         for i in data:
+            # find the host
             if i.startswith("Host: "):
                 host = "http://"+i[6:]
                 break
+        # if the http header is invalid
+        # or if the method is not allowed
         if len(http_request) != 3 or http_request[0] != "GET":
             msg = "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/html\r\n\r\n"
             self.request.sendall(msg.encode())
+        # get the requested path
         http_request = http_request[1]
+        # find the file
         root = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'www')
         if os.path.isfile(root+os.path.abspath(http_request)):
+            # format the response header when the request is valid
+            # HTTP code 200 OK
             msg = "HTTP/1.1 200 OK\r\nContent-Type: {}\r\n\r\n"
+            # MIME type for css file
             if http_request[-4:] == ".css":
                 msg = msg.format("text/css")
                 content = open(root+http_request).read()
                 msg = msg + content
                 self.request.sendall(msg.encode())
+            # MIME type for html file
             elif http_request[-5:] == ".html":
                 msg = msg.format("text/html")
                 content = open(root+http_request).read()
                 msg = msg + content
                 self.request.sendall(msg.encode())
+        # find the directory
         elif os.path.isdir(root+os.path.abspath(http_request)):
+            # redirect HTTP code 301
             if http_request[-1] != "/":
                 new = host + http_request + "/"
                 msg = "HTTP/1.1 301 Moved Permanently\r\nLocation: {}\r\n\r\n".format(
                     new)
                 self.request.sendall(msg.encode())
+            # HTTP code 200
             content = open(os.path.join(
                 root+http_request, "index.html")).read()
             msg = "HTTP/1.1 200 OK\r\nContent-Type: text/html\r\n\r\n"
             msg = msg + content
             self.request.sendall(msg.encode())
+        # if no such directory/file
         else:
             msg = "HTTP/1.1 404 Not Found\r\nContent-Type: text/html\r\n\r\n"
             content = """
                 <html>
                 <body>
-                <h2></h2><h1>Not Found</h1> The requested URL{} was not found on this server.
+                <h2></h2><h1>Not Found</h1> The requested URL {} was not found on this server.
                 </body>
                 </html>
             """.format(http_request)
